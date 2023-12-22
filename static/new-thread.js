10af0de029301f2814046096ffccbd3e8d94a95b
Sam <sams421@icloud.com>
2023-12-22 01:01:25 +0000

    Included a new thread iframe widget. Includes a POST request to make a new thread and the ability to exit the widget.

---
+++ static/new-thread.js
@@ -0,0 +1,47 @@
+submit_thread_element = document.getElementById("submit-thread")
+close_element = document.getElementById("close")
+
+
+submit_thread_element.addEventListener("click", submit_thread);
+close_element.addEventListener("click", close_doc);
+
+async function submit_thread() {
+    title_box = document.getElementById("title-box")
+    body_box = document.getElementById("body-box")
+
+
+    thread_title = title_box.value
+    thread_body = body_box.value
+
+    title_box.value = ''
+    body_box.value = ''
+
+    promise = await fetch("./createnewthread",
+    {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "title": thread_title,
+            "body": thread_body
+        })
+    })
+
+
+    await window.parent.update_thread_display()
+
+    close_doc()
+
+
+}
+
+
+
+
+function close_doc() {
+    
+    submit_thread_element.removeEventListener("click", submit_thread, true); // Succeeds
+    close_element.removeEventListener("click", close_doc, true);
+    window.parent.close_frame()
+}
