341532faa0e568719223e3d79b31ab848c3d4e5f
Sam <sams421@icloud.com>
2023-12-23 22:55:23 +0000

    Added support for images

--- backend.js
+++ backend.js
@@ -13,8 +13,7 @@ const date_map = {
   "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9,
   "Oct": 10, "Nov": 11, "Dec": 12
 }
-
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: 'static/uploads/' })
 
 const thread_db_fp = 'db\\thread_db.json'
 const thread_id_fp = 'db\\thread_id.json'
@@ -39,7 +38,7 @@ function item_count_to_page_count(num) {
 
 // Function that provides lookups for a dictionary and also provides a default
 function dict_get(object, key, default_value) {
-  var result = object[key];
+  const result = object[key];
   return (typeof result !== "undefined") ? result : default_value;
 }
 
@@ -61,10 +60,10 @@ function t_compareComments(a, b) {
   }
 }
 function t_compareSortNewest(a, b) {
-  var date_arr_a = datestring_to_arr(a[3]);
-  var date_arr_b = datestring_to_arr(b[3]);
+  const date_arr_a = datestring_to_arr(a[3]);
+  const date_arr_b = datestring_to_arr(b[3]);
 
-  for (var i = 0; i < date_arr_a.length; i++) {
+  for (let i = 0; i < date_arr_a.length; i++) {
     if (date_arr_a[i] > date_arr_b[i]) {
       return -1;
     }
@@ -75,10 +74,10 @@ function t_compareSortNewest(a, b) {
   return 0;
 }
 function t_compareSortOldest(a, b) {
-  var date_arr_a = datestring_to_arr(a[3]);
-  var date_arr_b = datestring_to_arr(b[3]);
+  const date_arr_a = datestring_to_arr(a[3]);
+  const date_arr_b = datestring_to_arr(b[3]);
 
-  for (var i = 0; i < date_arr_a.length; i++) {
+  for (let i = 0; i < date_arr_a.length; i++) {
     if (date_arr_a[i] < date_arr_b[i]) {
       return -1;
     }
@@ -88,7 +87,6 @@ function t_compareSortOldest(a, b) {
   }
   return 0;
 }
-
 function c_compareLikes(a, b) {
   if (a[3] === b[3]) {
     return 0;
@@ -98,10 +96,10 @@ function c_compareLikes(a, b) {
   }
 }
 function c_compareSortNewest(a, b) {
-  var date_arr_a = datestring_to_arr(a[2]);
-  var date_arr_b = datestring_to_arr(b[2]);
+  const date_arr_a = datestring_to_arr(a[2]);
+  const date_arr_b = datestring_to_arr(b[2]);
 
-  for (var i = 0; i < date_arr_a.length; i++) {
+  for (let i = 0; i < date_arr_a.length; i++) {
     if (date_arr_a[i] > date_arr_b[i]) {
       return -1;
     }
@@ -112,10 +110,10 @@ function c_compareSortNewest(a, b) {
   return 0;
 }
 function c_compareSortOldest(a, b) {
-  var date_arr_a = datestring_to_arr(a[2]);
-  var date_arr_b = datestring_to_arr(b[2]);
+  const date_arr_a = datestring_to_arr(a[2]);
+  const date_arr_b = datestring_to_arr(b[2]);
 
-  for (var i = 0; i < date_arr_a.length; i++) {
+  for (let i = 0; i < date_arr_a.length; i++) {
     if (date_arr_a[i] < date_arr_b[i]) {
       return -1;
     }
@@ -125,21 +123,32 @@ function c_compareSortOldest(a, b) {
   }
   return 0;
 }
+function c_compareSortHasImage(a, b) {
+  if (a[5] === '_' && b[5] === '_') {
+    return 0
+  }
+  else if (a[5] === '_') {
+    return 1
+  }
+  else {
+    return -1
+  }
+}
 
 
 
 // Converts an integer to an ID key used for looking up threads
 function int_to_id(x) {
-  var int_string = x.toString();
-  var padding = '0'.repeat(8 - int_string.length);
-  var id_string = padding.concat(int_string);
+  const int_string = x.toString();
+  const padding = '0'.repeat(8 - int_string.length);
+  const id_string = padding.concat(int_string);
   return id_string;
 }
 
 // Returns an array representing the current time and date
 function get_date_arr() {
-  var date = new Date()
-  var date_arr = []
+  let date = new Date()
+  let date_arr = []
   date = date.toString()
   date = date.split(" ")
   const time = date[4].split(":")
@@ -151,10 +160,9 @@ function get_date_arr() {
 
 // Converts a date array into a string, or vice versa.
 function datearr_to_string(a) {
-  var s = ''
-  for (var i = 0; i < a.length; i++) {
-
-    a_i_string = a[i].toString()
+  let s = ''
+  for (let i = 0; i < a.length; i++) {
+    let a_i_string = a[i].toString()
     if (i > 0) {
       if (a_i_string.length < 2) {
         a_i_string = '0'.concat(a_i_string)
@@ -168,9 +176,9 @@ function datearr_to_string(a) {
   return s
 }
 function datestring_to_arr(a) {
-  var s_arr = a.split("-")
-  var b = []
-  for (var i = 0; i < s_arr.length; i++) {
+  const s_arr = a.split("-")
+  let b = []
+  for (let i = 0; i < s_arr.length; i++) {
     b.push(parseInt(s_arr[i]))
   }
   return b;
@@ -179,13 +187,23 @@ function datestring_to_arr(a) {
 
 // Returns the JSON representation of a given JSON filepath
 async function open_json_file(filepath) {
-  var promise = await fspromise.readFile(filepath)
+  const promise = await fspromise.readFile(filepath)
   return JSON.parse(promise.toString())
 
 }
 
+// Checks if a file exists
+function check_file_exists(file) {
+  return fs.promises.access(file, fs.constants.F_OK)
+           .then(() => true)
+           .catch(() => false)
+}
 
-
+function delete_image(file) {
+  if (check_file_exists(image_file_path)) {
+    fspromise.unlink(image_file_path)
+  }
+}
 
 
 
@@ -196,42 +214,44 @@ app.get("/", function (req, res) {
 
 // Returns the number of thread/comment pages available to view
 app.get("/getpagecount", async function (req, res) {
-  var page_count = 0
+  let page_count = 0
 
   // Reads the thread_db file and returns the number of pages
-  var files = await open_json_file(thread_db_fp)
-  var page_count = item_count_to_page_count(files["threads"].length)
+  const files = await open_json_file(thread_db_fp)
+  page_count = item_count_to_page_count(files["threads"].length)
   res.send({ 'page_count': page_count })
 });
 app.get("/getcommentcount", async function (req, res) {
-  var thread_id = req.query["thread-id"]
-  var comment_count = 0
+  const thread_id = req.query["thread-id"]
+  let comment_count = 0
 
   // Reads the thread_db file and returns the number of pages
-  var files = await open_json_file(comment_db_fp)
+  const files = await open_json_file(comment_db_fp)
   for (let i = 0; i < files["comments"].length; i++) {
     if (files.comments[i]["parent"] == thread_id) {
       comment_count += 1
     }
   }
 
-
-  var page_count = item_count_to_page_count(comment_count)
-
-  res.send({ 'page_count': page_count })
+  const page_count = item_count_to_page_count(comment_count)
+  res.send({'comment_count':comment_count, 'page_count': page_count })
 });
 
 
 // Returns the five threads/comments that satisfy the user's request
 app.get("/threads", async function (req, res) {
-  var select = dict_get(req.query, "select", "date-newest");
-  var search = dict_get(req.query, "search", "");
-  var page = parseInt(dict_get(req.query, "page", '1'));
 
-  var files = await open_json_file(thread_db_fp);
-  var thread_arr = files["threads"]
-  var arr = []
+  // Getting parameters from request
+  const select = dict_get(req.query, "select", "date-newest");
+  const search = dict_get(req.query, "search", "");
+  const page = parseInt(dict_get(req.query, "page", '1'));
 
+  // Opening the threads database 
+  const files = await open_json_file(thread_db_fp);
+  const thread_arr = files["threads"]
+  let arr = []
+
+  // Getting all threads that include the search result and placing into an array
   for (let i = 0; i < thread_arr.length; i++) {
     if (thread_arr[i]["title"].includes(search)) {
       arr.push([thread_arr[i]["id"], thread_arr[i]["title"], thread_arr[i]["body"], thread_arr[i]["date"],
@@ -253,10 +273,11 @@ app.get("/threads", async function (req, res) {
     arr.sort(t_compareComments);
   }
 
-  var start = (page - 1) * 5
-  var end = Math.min(page * 5, arr.length)
+  // Slicing the array to return 5 items
+  const start = (page - 1) * 5
+  const end = Math.min(page * 5, arr.length)
 
-  items = arr.slice(start, end)
+  let items = arr.slice(start, end)
   while (items.length < 5) {
     items.push(['_', '_', '_', '_', '_', '_'])
   }
@@ -264,21 +285,22 @@ app.get("/threads", async function (req, res) {
   res.send(JSON.stringify(items))
 })
 app.get("/comments", async function (req, res) {
-  var select = dict_get(req.query, "select", "date-newest");
-  var search = dict_get(req.query, "search", "");
-  var page = parseInt(dict_get(req.query, "page", '1'));
-  var thread_id = req.query["thread-id"]
+  const select = dict_get(req.query, "select", "date-newest");
+  const search = dict_get(req.query, "search", "");
+  const page = parseInt(dict_get(req.query, "page", '1'));
+  const thread_id = req.query["thread-id"]
 
-  var files = await open_json_file(comment_db_fp);
-  var comment_arr = files["comments"]
-  var arr = []
+  const files = await open_json_file(comment_db_fp);
+  const comment_arr = files["comments"]
+  let arr = []
 
   for (let i = 0; i < comment_arr.length; i++) {
     if (comment_arr[i]["body"].includes(search) && comment_arr[i]["parent"] == thread_id) {
       arr.push([comment_arr[i]["id"], comment_arr[i]["body"], comment_arr[i]["date"],
-      comment_arr[i]["likes"], comment_arr[i]["parent"]])
+      comment_arr[i]["likes"], comment_arr[i]["parent"], comment_arr[i]["image"]])
     }
   }
+
   // Sorting the array
   if (select == 'date-newest') {
     arr.sort(c_compareSortNewest);
@@ -289,11 +311,14 @@ app.get("/comments", async function (req, res) {
   else if (select == 'likes') {
     arr.sort(c_compareLikes);
   }
+  else if (select == 'has-image') {
+    arr.sort(c_compareSortHasImage)
+  }
 
-  var start = (page - 1) * 5
-  var end = Math.min(page * 5, arr.length)
+  const start = (page - 1) * 5
+  const end = Math.min(page * 5, arr.length)
 
-  items = arr.slice(start, end)
+  let items = arr.slice(start, end)
   while (items.length < 5) {
     items.push(['_', '_', '_', '_', '_', '_'])
   }
@@ -358,41 +383,53 @@ app.post("/createnewthread", async function (req, res) {
   fspromise.writeFile(thread_db_fp, JSON.stringify(jsonData));
   res.json({})
 });
-app.post("/addcomment", async function (req, res) {
+app.post("/addcomment", upload.single("file"), async function (req, res) {
 
-  // Getting variables from the body
-  var thread_id = req.body["thread-id"]
-  var comment_body = req.body["body"];
 
   // Opening the ID database and getting the least-recently used ID. 
   // This is the equivalent of generating a primary key in a database.
+
   var jsonData = await open_json_file(comment_id_fp)
   var id_int = parseInt(jsonData["id"])
   var new_id_int = id_int + 1
   jsonData["id"] = new_id_int
-
   fspromise.writeFile(comment_id_fp, JSON.stringify(jsonData));
-
-  // Gets the correct ID string to add to the thread
   var id_string = int_to_id(id_int);
 
-  // Getting the date that the comment was posted
-  var date_string = datearr_to_string(get_date_arr())
+  if (req["file"] === undefined) {
+    var img_path = '_'
+  }
+  else {
+    var tempPath = req.file.path;
+    var suffix = path.extname(req.file.originalname).toLowerCase()
+    if (!(suffix === ".png" || suffix === ".jpg" || suffix === '.jpeg')) {
+      res.status(400);
+    }
+    var targetPath = path.join(__dirname, `./static/uploads/${id_string}.jpg`);
+    promise = await fspromise.rename(tempPath, targetPath)
+    var img_path =`./uploads/${id_string}.jpg`
+  }
 
-  // Setting likes and comments
+
+  var thread_id = req.body["thread-id"]
+  var comment_body = req.body["comment-body"]
+
+  // Adding comment to db
+  var date_string = datearr_to_string(get_date_arr());
   var likes = 0;
-
-  // Opening comment database
   var jsonData = await open_json_file(comment_db_fp)
+
   jsonData.comments.push({
     "id": id_string,
     "body": comment_body,
     "date": date_string,
     "likes": likes,
     "parent": thread_id,
+    "image": img_path,
   })
 
-  fspromise.writeFile(comment_db_fp, JSON.stringify(jsonData));
+
+  await fspromise.writeFile(comment_db_fp, JSON.stringify(jsonData));
 
   // Adding 1 comment to thread database
   var threadJsonData = await open_json_file(thread_db_fp)
@@ -402,14 +439,11 @@ app.post("/addcomment", async function (req, res) {
     }
   }
 
-  fspromise.writeFile(thread_db_fp, JSON.stringify(threadJsonData));
-
-
-  res.json({})
+  await fspromise.writeFile(thread_db_fp, JSON.stringify(threadJsonData));
+  res.json({});
 });
 
 
-// Deletes a given thread/comment
 app.post("/deletethread", async function (req, res) {
   var thread_deletion_id = req.body["thread-id"]
 
@@ -437,6 +471,12 @@ app.post("/deletethread", async function (req, res) {
 
   while (inds_to_delete.length > 0) {
     ind = inds_to_delete.pop()
+    image_src = jsonData.comments[ind]["image"]
+    if (image_src !== '_') {
+      comment_id = jsonData.comments[ind]["id"]
+      image_file_path = `./static/uploads/${comment_id}.jpg`
+      delete_image(image_file_path);
+    } 
     jsonData.comments.splice(ind, 1);
   }
 
@@ -476,7 +516,10 @@ app.post("/deletecomment", async function (req, res) {
 
   fspromise.writeFile(thread_db_fp, JSON.stringify(threadJsonData));
 
-
+  // Deleting image (if it exists)
+  image_file_path = `./static/uploads/${comment_deletion_id}.jpg`
+  delete_image(image_file_path);
+  
   res.json({})
 
 });
@@ -524,31 +567,6 @@ app.post("/likecomment", async function (req, res) {
 
 });
 
-app.post("/uploadimage", upload.single("file"), async function (req, res) {
-  
-
-  var tempPath = req.file.path;
-  
-  var suffix = path.extname(req.file.originalname).toLowerCase()
-
-  var thread_id = req.body["hidden-thread-id"]
-
-  // Opening the ID database and getting the least-recently used ID. 
-  // This is the equivalent of generating a primary key in a database.
-  var jsonData = await open_json_file(comment_id_fp)
-  var id_int = parseInt(jsonData["id"])
-  var new_id_int = id_int + 1
-  jsonData["id"] = new_id_int
-  fspromise.writeFile(comment_id_fp, JSON.stringify(jsonData));
-  var id_string = int_to_id(id_int);
-
-  var targetPath = path.join(__dirname, `./uploads/${id_string}.png`);
-
-  if (suffix === ".png" || suffix === ".jpg") {
-    promise = await fspromise.rename(tempPath, targetPath)
-  }
-  res.status(200);
-});
 
 
 app.listen(port, function () {
