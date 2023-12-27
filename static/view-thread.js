191943b005e28da5c91dab79c47ba677c3ef436e
Sam <sams421@icloud.com>
2023-12-27 00:34:29 +0000

    Linted view-thread, added exception handling for faulty API calls, improved display

--- static/view-thread.js
+++ static/view-thread.js
@@ -1,16 +1,16 @@
-var thread_id = window.parent.selected_thread
+let thread_id = window.parent.selected_thread
 
-element_close = document.getElementById("view-thread-close")
-element_delete_thread_button = document.getElementById("delete-thread")
-element_like_thread_button = document.getElementById("like-thread")
-element_dislike_thread_button = document.getElementById("dislike-thread")
-element_get_comments = document.getElementById("get-comments")
-element_back_comment_page = document.getElementById("back-comment-page")
-element_next_comment_page = document.getElementById("next-comment-page")
-element_submit_comment_button = document.getElementById("submit-comment")
+const element_close = document.getElementById("view-thread-close")
+const element_delete_thread_button = document.getElementById("delete-thread")
+const element_like_thread_button = document.getElementById("like-thread")
+const element_dislike_thread_button = document.getElementById("dislike-thread")
+const element_get_comments = document.getElementById("get-comments")
+const element_back_comment_page = document.getElementById("back-comment-page")
+const element_next_comment_page = document.getElementById("next-comment-page")
+const element_submit_comment_button = document.getElementById("submit-comment")
 
-var form_comment_page = 1;
-var max_comment_page = 1;
+let form_comment_page = 1;
+let max_comment_page = 1;
 
 element_close.addEventListener("click", close_doc);
 element_delete_thread_button.addEventListener("click", delete_thread);
@@ -22,7 +22,7 @@ element_next_comment_page.addEventListener("click", increment_form_comment_page)
 element_submit_comment_button.addEventListener("click", submit_comment);
 
 
-var thread_likes = 0
+let thread_likes = 0
 
 
 get_thread_info()
@@ -32,38 +32,51 @@ update_comment_display();
 
 
 async function get_thread_info() {
-    var promise = await fetch("./threadinfo?" + new URLSearchParams({
-        'id': thread_id,
-    }), { method: "GET" })
+    try {
 
-    var result = await promise.json()
 
-    thread_likes = result["likes"]
+        const promise = await fetch("./threadinfo?" + new URLSearchParams({
+            'id': thread_id,
+        }), { method: "GET" })
 
-    
+        const result = await promise.json()
 
-    document.getElementById("view-thread-title").innerHTML = result["title"]
-    document.getElementById("view-thread-body").innerHTML = result["body"]
-    document.getElementById("thread-date").innerHTML = window.parent.date_to_readable(result["date"])
-    document.getElementById("thread-last-update").innerHTML = window.parent.date_to_readable(result["lastupdate"])
-    document.getElementById("thread-likes").innerHTML = thread_likes
+        thread_likes = result["likes"]
+
+
+
+        document.getElementById("view-thread-title").innerHTML = result["title"]
+        document.getElementById("view-thread-body").innerHTML = result["body"]
+        document.getElementById("thread-date").innerHTML = window.parent.date_to_readable(result["date"])
+        document.getElementById("thread-last-update").innerHTML = window.parent.date_to_readable(result["lastupdate"])
+        document.getElementById("thread-likes").innerHTML = thread_likes
+
+    }
+    catch {
+        alert("Network error: /threadinfo failed to execute")
+    }
 
 }
 
 async function delete_thread() {
-    promise = await fetch("./deletethread",
-        {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-                "thread-id": thread_id,
+    try {
+        await fetch("./deletethread",
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    "thread-id": thread_id,
+                })
             })
-        })
 
-    await window.parent.update_thread_display()
-    close_doc();
+        await window.parent.update_thread_display()
+        close_doc();
+    }
+    catch {
+        alert("Network error: /deletethread failed to execute")
+    }
 
 }
 
@@ -80,20 +93,27 @@ async function dislike_thread() {
 }
 
 async function update_thread_likes(val) {
-    promise = await fetch("./likethread",
-        {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-                "thread-id": thread_id,
-                "like-number": val,
+    try {
+
+
+        await fetch("./likethread",
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    "thread-id": thread_id,
+                    "like-number": val,
+                })
             })
-        })
 
-    window.parent.update_thread_display()
+        window.parent.update_thread_display()
 
+    }
+    catch {
+        alert("Network error: /likethread failed to execute")
+    }
 }
 
 // // Increments the thread page we are currently on, and modifies the form_page attribute
@@ -121,49 +141,54 @@ function update_comment_form_max() {
 
 // // Updates the table with data given
 function update_comment_table(data) {
-    var data_arr = []
-    for (var i in data) {
-        var temp_arr = []
-        for (var j in data[i]) {
+    let data_arr = []
+    for (let i in data) {
+        let temp_arr = []
+        for (let j in data[i]) {
             temp_arr.push(data[i][j])
         }
         data_arr.push(temp_arr);
     }
 
     for (let i = 0; i < 5; i++) {
-        body_element = document.getElementById(`body-comment-${i + 1}`)
-        id_element = document.getElementById(`id-comment-${i + 1}`)
-        date_element = document.getElementById(`date-comment-${i + 1}`)
-        likes_element = document.getElementById(`likes-comment-${i + 1}`)
-        div_element = document.getElementById(`image-div-${i + 1}`)
+        const body_element = document.getElementById(`body-comment-${i + 1}`)
+        const id_element = document.getElementById(`id-comment-${i + 1}`)
+        const date_element = document.getElementById(`date-comment-${i + 1}`)
+        const likes_element = document.getElementById(`likes-comment-${i + 1}`)
+        const div_element = document.getElementById(`image-div-${i + 1}`)
+        const table_row = document.getElementById(`comment-row-${i + 1}`)
 
         body_element.innerHTML = data_arr[i][1]
         id_element.innerHTML = data_arr[i][0]
-        date_element.innerHTML = data_arr[i][2]
+        date_element.innerHTML = window.parent.date_to_readable(data_arr[i][2])
         likes_element.innerHTML = data_arr[i][3]
 
-        
+
         if (data_arr[i][5] == '_') {
             div_element.innerHTML = `<label>${data_arr[i][5]}</label>`
+            div_element.style.display = "none"
         }
         else {
+            div_element.style.display = "inherit"
             div_element.innerHTML = `<img src='${data_arr[i][5]}'>`
+
         }
 
-        dislike_element = document.getElementById(`dislike-comment-row${i + 1}`)
-        like_element = document.getElementById(`like-comment-row${i + 1}`)
-        delete_element = document.getElementById(`delete-comment-row${i + 1}`)
+        const dislike_element = document.getElementById(`dislike-comment-row${i + 1}`)
+        const like_element = document.getElementById(`like-comment-row${i + 1}`)
+        const delete_element = document.getElementById(`delete-comment-row${i + 1}`)
 
         if (data_arr[i][0] == '_') {
             dislike_element.innerHTML = ''
             like_element.innerHTML = ''
             delete_element.innerHTML = ''
+            table_row.style.display = 'none'
         }
         else {
             dislike_element.innerHTML = '-'
             like_element.innerHTML = '+'
-            delete_element.innerHTML = 'X'
-
+            delete_element.innerHTML = 'Delete'
+            table_row.style.display = 'initial'
         }
 
 
@@ -177,74 +202,96 @@ function update_comment_table(data) {
 // // Updates which threads are currently shown to the user
 async function update_comment_display() {
 
-    var select_option = document.getElementById("sortby-comments")
-    var select_value = select_option.options[select_option.selectedIndex].value;
-    var search_item = document.getElementById("search-comments").value
+    const select_option = document.getElementById("sortby-comments")
+    const select_value = select_option.options[select_option.selectedIndex].value;
+    const search_item = document.getElementById("search-comments").value
+
+    try {
 
 
-    var promise = await fetch("./comments?" + new URLSearchParams({
-        'select': select_value,
-        'search': search_item,
-        'page': form_comment_page,
-        'thread-id': thread_id,
-    }), { method: "GET" })
+
+        const promise = await fetch("./comments?" + new URLSearchParams({
+            'select': select_value,
+            'search': search_item,
+            'page': form_comment_page,
+            'thread-id': thread_id,
+        }), { method: "GET" })
 
 
-    result_json = await promise.json()
-    update_comment_table(result_json)
-    update_comment_form_max()
+        const result_json = await promise.json()
+        update_comment_table(result_json)
+        update_comment_form_max()
+    }
+    catch {
+        alert("Network error: /comments failed to execute")
+    }
 }
 
 // // Gets the number of pages of threads in the application
 async function get_comment_count() {
+    try {
 
-    var promise = await fetch("./getcommentcount?" + new URLSearchParams({
-        'thread-id': thread_id,
-    }), { method: "GET" })
 
-    json_obj = await promise.json()
-    document.getElementById("max-comment-page").innerHTML = json_obj["page_count"]
+        const promise = await fetch("./getcommentcount?" + new URLSearchParams({
+            'thread-id': thread_id,
+        }), { method: "GET" })
+
+        const json_obj = await promise.json()
+        document.getElementById("max-comment-page").innerHTML = json_obj["page_count"]
+
+
+    }
+    catch {
+        alert("Network error: /getcommentcount failed to execute")
+    }
 }
 
 async function submit_comment() {
 
     const form = document.getElementById("submit-comment-form")
     const formData = new FormData(form);
-    formData.append("thread-id",thread_id);
+    formData.append("thread-id", thread_id);
 
 
     document.getElementById("new-comment-box").value = ''
     document.getElementById("upload-file").value = ''
 
+    try {
 
-    promise = await fetch("./addcomment",
-        {
-            method: "POST",
-            body: formData
-        })
 
-    json_obj = await promise.json()
-    if (json_obj["error"] == "content") {
-        alert("No body/image provided")
+        const promise = await fetch("./addcomment",
+            {
+                method: "POST",
+                body: formData
+            })
+
+        const json_obj = await promise.json()
+        if (json_obj["error"] == "content") {
+            alert("No body/image provided")
+        }
+        else if (json_obj["error"] == "file") {
+            alert("Filetype not supported")
+        }
+
+        get_last_thread_update()
+        await window.parent.update_thread_display()
+
+        await update_comment_display()
+
     }
-    else if (json_obj["error"] == "file") {
-        alert("Filetype not supported")
+    catch {
+        alert("Network error: /addcomment failed to execute")
     }
-
-    get_last_thread_update()
-    await window.parent.update_thread_display()
-
-    await update_comment_display()
 
 }
 
 async function like_comment(pos, likes) {
-    comment_id = document.getElementById(`id-comment-${pos}`).innerHTML
+    const comment_id = document.getElementById(`id-comment-${pos}`).innerHTML
     if (comment_id == '_') {
         return;
     }
-    like_count_element = document.getElementById(`likes-comment-${pos}`)
-    like_count = parseInt(like_count_element.innerHTML)
+    const like_count_element = document.getElementById(`likes-comment-${pos}`)
+    const like_count = parseInt(like_count_element.innerHTML)
 
     await update_comment_likes(comment_id, likes)
     like_count_element.innerHTML = like_count + likes
@@ -261,7 +308,7 @@ async function delete_comment_check(pos) {
     }
 
 
-    comment_id = document.getElementById(`id-comment-${pos}`).innerHTML
+    const comment_id = document.getElementById(`id-comment-${pos}`).innerHTML
     if (comment_id == '_') {
         return;
     }
@@ -274,43 +321,63 @@ async function delete_comment_check(pos) {
 
 
 async function update_comment_likes(id, val) {
-    promise = await fetch("./likecomment",
-        {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-                "comment-id": id,
-                "like-number": val,
+    try {
+
+
+        await fetch("./likecomment",
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    "comment-id": id,
+                    "like-number": val,
+                })
             })
-        })
+    }
+    catch {
+        alert("Network error: /likecomment failed to execute")
+    }
 
 }
 
 async function delete_comment(comment_id) {
-    promise = await fetch("./deletecomment",
-        {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-                "comment-id": comment_id,
+    try {
+        await fetch("./deletecomment",
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    "comment-id": comment_id,
+                })
             })
-        })
+    }
+    catch {
+        alert("Network error: /deletecomment failed to execute")
+    }
 
 
 
 }
 
 async function get_last_thread_update() {
-    var promise = await fetch("./getlastupdate?" + new URLSearchParams({
-        'thread-id': thread_id,
-    }), { method: "GET" })
+    try {
 
-    json_obj = await promise.json()
-    document.getElementById("thread-last-update").innerHTML = json_obj["last_update"]
+
+        const promise = await fetch("./getlastupdate?" + new URLSearchParams({
+            'thread-id': thread_id,
+        }), { method: "GET" })
+
+        const json_obj = await promise.json()
+        document.getElementById("thread-last-update").innerHTML = json_obj["last_update"]
+
+    }
+    catch {
+        alert("Network error: /getlastupdate failed to return values")
+    }
 }
 
 
