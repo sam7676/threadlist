5ee3697d829c849cbcf24ac6c7183892a781695c
Sam <sams421@icloud.com>
2023-12-28 21:29:21 +0000

    Added tests for backend.js

--- backend.test.js
+++ backend.test.js
@@ -1,21 +1,44 @@
 const backend = require('./backend')
 
-test('checks type of 4', () => {
-    expect(backend.check_type(4, backend.NUMBER_TYPE)).toBe();
+
+test('check_valid_id', () => {
+    expect(backend.check_valid_id("00000001")).toBe(true);
+    expect(backend.check_valid_id("0000002")).toBe(false);
+    expect(backend.check_valid_id(3)).toBe(false);
   });
 
-test('checks non negative object', () => {
-    expect(backend.check_non_negative(5)).toBe();
+// Note function only called with nonnegative integers from the back end
+test('item_count_to_page_count', () => {
+  expect(backend.item_count_to_page_count(0)).toBe(1);
+  expect(backend.item_count_to_page_count(1)).toBe(1);
+  expect(backend.item_count_to_page_count(5)).toBe(1);
+  expect(backend.item_count_to_page_count(6)).toBe(2);
+  expect(backend.item_count_to_page_count(10)).toBe(2);
 });
 
+test('dict_get', () => {
+  const d = {1:1,2:2,3:3,4:4,5:5,6:6}
+  expect(backend.dict_get(d,1,10)).toBe(1);
+  expect(backend.dict_get(d,7,10)).toBe(10);
+  expect(backend.dict_get(d,1,7)).toBe(1);
+  expect(backend.dict_get(d,1,7)).toBe(1);
+  expect(backend.dict_get(d,1,7)).toBe(1);
+  expect(backend.dict_get(1, 1, 7)).toBe(7);
+});
 
-// test('checks non negative object', () => {
-//     expect(backend.check_non_negative(-1)).toThrow();
-// });
+test('int_to_id', () => {
+  expect(backend.int_to_id(0)).toBe('00000000');
+  expect(backend.int_to_id(1)).toBe('00000001');
+  expect(backend.int_to_id(100)).toBe('00000100');
+  expect( () => {
+        backend.int_to_id(-1)
+      }).toThrow(Error);
+  expect( () => {
+        backend.int_to_id(100000000)
+      }).toThrow(Error);
+});
 
-
-test("Test description", () => {
-    expect(backend.check_non_negative(-1)).toThrow(TypeError);
-    expect(backend.check_non_negative(-1)).toThrow("UNKNOWN ERROR");
-    expect(backend.check_non_negative(-1)).toThrow("Number must be non-negative");
-  });
+test('check_file_exists', async () => {
+  expect(await backend.check_file_exists("./backend.js")).toBe(true);
+  expect(await backend.check_file_exists("a")).toBe(false);
+});
